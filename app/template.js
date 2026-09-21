86e9a0af6d3b9d949e62312ed64b65ef1c45d2ee
skylinegtr030-web <skylinegtr030@gmail.com>
2026-09-21 20:38:44 +0300

    refactor: remove capabilities grid section

--- app/template.js
+++ app/template.js
@@ -5,6 +5,7 @@ import './vivid-backgrounds.css';
 import './lab-aggressive.css';
 import './lab-motion.css';
 import './modern-polish.css';
+import './remove-capabilities.css';
 
 export default function Template({ children }) {
   return children;
