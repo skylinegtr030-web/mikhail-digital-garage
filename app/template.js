c1ed3d7ad5bfe036eb4dc344e6f106205344cf6f
skylinegtr030-web <skylinegtr030@gmail.com>
2026-09-21 20:07:20 +0300

    style: aggressive brutalist transformation lab

--- app/template.js
+++ app/template.js
@@ -2,6 +2,7 @@ import './theme-overrides.css';
 import './shape-modes.css';
 import './performance.css';
 import './vivid-backgrounds.css';
+import './lab-aggressive.css';
 
 export default function Template({ children }) {
   return children;
