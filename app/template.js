4e85d355a0b525fb022eafb2e5b4e71cae5a25b3
skylinegtr030-web <skylinegtr030@gmail.com>
2026-09-21 20:54:07 +0300

    style: premium polish for capability engine

--- app/template.js
+++ app/template.js
@@ -7,6 +7,7 @@ import './lab-motion.css';
 import './modern-polish.css';
 import './remove-capabilities.css';
 import './process-overdrive.css';
+import './capability-polish.css';
 
 export default function Template({ children }) {
   return children;
