f2e17ca9fd61301cf23b3d19ea9cd5944988654f
skylinegtr030-web <skylinegtr030@gmail.com>
2026-09-21 19:53:13 +0300

    style: richer reactive background atmospheres

--- app/template.js
+++ app/template.js
@@ -1,6 +1,7 @@
 import './theme-overrides.css';
 import './shape-modes.css';
 import './performance.css';
+import './vivid-backgrounds.css';
 
 export default function Template({ children }) {
   return children;
